082c92b5cbff19a19f78d25066b3e2d48f279647
Xinghua Pan <xi.pan@ufl.edu>
2020-11-30 20:21:36 -0500

    zipf distribution fun for followers

---
+++ DOSP.P4.Client/Utils.fs
@@ -0,0 +1,25 @@
+// Name: Xinghua Pan
+// UFID: 95160902
+namespace DOSP.P4.Client
+
+module Utils =
+    open System
+    open MathNet.Numerics.Distributions
+
+    let a = 1.2 // zipf a param
+    let getZipF (total: int) = Zipf(a, total)
+
+    let getZipfFollower (uids: string list) =
+        let uidsLen = uids.Length
+        let zipf = getZipF uidsLen
+        let rnd = Random()
+        uids
+        |> List.map (fun uid ->
+            let numOfFollowers = zipf.Sample()
+
+            let followers =
+                [ 1 .. numOfFollowers ]
+                |> List.map (fun idx -> List.item (rnd.Next(uidsLen)) uids)
+                |> Set.ofList
+
+            (uid, followers))
--- DOSP.P4.Common/Messages/EngineResp.fs
+++ DOSP.P4.Common/Messages/EngineResp.fs
@@ -8,4 +8,4 @@ module EngineResp =
     type EngineResp<'a> = { RType: RespType; Body: 'a }
 
     let RespSucc (msg: 'a) = { RType = Succ; Body = msg }
-    let RespFail (msg: 'a) = { RType = Succ; Body = msg }
+    let RespFail (msg: 'a) = { RType = Fail; Body = msg }
